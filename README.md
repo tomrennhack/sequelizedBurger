c22ad050511881bad9690c52d0d9bf7621434b96
tomrennhack <tomrennhack@gmail.com>
2019-11-03 12:01:47 -0500

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# eat-da-burger
+eat-da-burger
---
+++ schema.sql
@@ -0,0 +1,18 @@
+-- DROP DATABASE IF EXISTS burgers_db;
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
+
+-- CREATE TABLE burgers (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     burger_name VARCHAR(255) NOT NULL,
+--     devoured BOOLEAN DEFAULT false,
+--     PRIMARY KEY (id)
+-- );
+
+-- INSERT INTO burgers (burger_name, devoured) 
+-- VALUES
+--     ("Cheeseburger", false), 
+--     ("Bacon Cheeseburger", false),
+--     ("Impossible Burger", false),
+--     ("California Veggie Burger", false);
+
